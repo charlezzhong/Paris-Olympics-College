7e5800a32ee523ab3120a7d2bd50075ee84c2344
charlezzhong <zheqin@umich.edu>
2024-07-25 17:20:40 -0400

    more adjustment, add olympic picture

--- olympics/src/HomePage.js
+++ olympics/src/HomePage.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 import Filters from './Filters';
-import Results from './Results';
-import styles from './Homepage.module.css';
+import styles from './HomePage.module.css';
+import olympicImage from './olympics_ring.png';
+import mainFeature from './usain_bolt.jpeg';
 
 const HomePage = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -10,19 +11,17 @@ const HomePage = () => {
     sport: '',
     country: ''
   });
-  const [athletes, setAthletes] = useState([]);
-
-  const handleSearch = async () => {
-    // Fetch data from backend based on searchTerm and filters
-    // Update athletes state with fetched data
-  };
 
   return (
     <div className={styles.homePage}>
-      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <Filters filters={filters} setFilters={setFilters} />
-      <button className={styles.searchButton} onClick={handleSearch}>Search</button>
-      <Results athletes={athletes} />
+      <div className={styles.banner}>
+        <img src={olympicImage} alt="Olympic Games" className={styles.olympicImage} />
+        {/* Add more images as needed */}
+      </div>
+      <div className={styles.searchArea}>
+        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <Filters filters={filters} setFilters={setFilters} />
+      </div>
     </div>
   );
 };
